6cda23ba7dd346e887c5d29fe9416cd8db7a9a2d
progreccor <boss@delo-design.ru>
2026-08-27 20:29:40 +0300

    Update supported MariaDB LTS versions

--- ri.sh
+++ ri.sh
@@ -1125,35 +1125,31 @@ if ! grep -q "MYSQLPASS" ~/.bashrc; then
     cd /etc/yum.repos.d/
 
     echo
-    echo -e "${GREEN}MariaDB${WHITE} на данный момент имеет 5 релизов с долгосрочной поддержкой:"
-    echo "10.6  со сроком поддержки до 6 июля 2026"
+    echo -e "${GREEN}MariaDB${WHITE} на данный момент имеет 4 релиза с долгосрочной поддержкой:"
     echo "10.11 со сроком поддержки до 16 февраля 2028"
     echo -e "${GREEN}11.4${WHITE}  со сроком поддержки до 29 мая 2029"
     echo "11.8  со сроком поддержки до 4 июня 2028"
-    echo "12.3  со сроком поддержки до мая 2029"
+    echo "12.3  со сроком поддержки до 12 июня 2029"
     echo
     echo "Какой релиз ставить?"
     while true; do
       Maria_Version_Custom=0
-      vertical_menu "current" 2 0 10 default=2 "MariaDB 10.6" "MariaDB 10.11" "MariaDB 11.4" "MariaDB 11.8" "MariaDB 12.3" "Ввести другую версию вручную"
+      vertical_menu "current" 2 0 10 default=1 "MariaDB 10.11" "MariaDB 11.4" "MariaDB 11.8" "MariaDB 12.3" "Ввести другую версию вручную"
       choice=$?
       case "$choice" in
       0)
-        Maria_Version="10.6"
-        ;;
-      1)
         Maria_Version="10.11"
         ;;
-      2)
+      1)
         Maria_Version="11.4"
         ;;
-      3)
+      2)
         Maria_Version="11.8"
         ;;
-      4)
+      3)
         Maria_Version="12.3"
         ;;
-      5)
+      4)
         Maria_Version_Custom=1
         echo -e -n "Введите версию MariaDB в формате ${GREEN}X.Y${WHITE} (пустая строка для возврата в меню): ${GREEN}"
         read -r -e Maria_Version
